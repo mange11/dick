936104893d5c7b7b8ddbf33d87a35271eb2dcc22
mange11 <mangegud@gmail.com>
2020-03-07 09:52:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import turtle
+
+qazi_turtle = turtle.Turtle()
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
